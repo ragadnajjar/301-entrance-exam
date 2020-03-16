0372c43a2ce936f35b796c516e0d5398625ab382
ragadnajjar <ragadnajjar9@gmail.com>
2020-03-16 14:51:00 +0200

    q3

---
+++ js/q3.js
@@ -0,0 +1,32 @@
+
+function form(likes,date){
+    this.likes=likes;
+    this.date=date
+}
+form.prototype.render=function()
+function setitem(){
+    for (var i =0;i <form.length; i++){
+    }
+    localStorage.setItem('form',json.stringify(form));
+}
+function getitem(){
+    var getitem=localStorage.getItem('form');
+    json.parse(get);
+}
+function renderlist(){
+    var ulEl=document.getElementById('list');
+    for(i=0 ; i<forms.length ; i++){
+        var liEl=document.createElement('li')
+        liEl.textContent='${forms[i].likes} , ${forms[i].date}'
+        ulEl.appendChild(liEl);
+    }
+}
+
+ var myForm=document.getElementById('myform');
+myForm.addEventListener("submit", function(event){
+    var likes = event.target.likes.value;
+    var date = event.value.date.value;
+var newlikes=newlikes(likes,date);
+newnewlikes.render();
+});
+function myForm(){}
