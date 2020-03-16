bc430a201307ce61154f9a6949b52ef7854a9bc3
ragadnajjar <ragadnajjar9@gmail.com>
2020-03-16 12:47:52 +0200

    q1

---
+++ js/q1.js
@@ -0,0 +1,16 @@
+'use strict'
+// var b = 2;
+// var a = 1;
+// functionsum (a+b);{
+//     var sum a+b
+//     console.log (sum);
+//     return [a+b , 'the sum of ${a} and ${b} is ${sum} '];
+//     test sum(1+2);
+
+// }
+var Numbers = [];
+Number[1] = "2";
+Number[2] = "3";
+document.getElementById("demo").innerHTML = 
+Number[2] + person.length;
+console.log(Numbers);
